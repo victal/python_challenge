20772ba0f900c769c0509dc1efc467a4f4cb3ee1
Guilherme Victal <guilhermevictal@gmail.com>
2012-05-30 00:43:36 -0300

    done level 7

--- 7.py
+++ 7.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python2
-import pickle
-import urllib
+import zipfile
+import urllib.request
+import io
 
 #http://www.pythonchallenge.com/pc/def/channel.zip
-a = pickle.load(urllib.urlopen("http://www.pythonchallenge.com/pc/def/banner.p"))
-print(zip(a))
+zip_file = zipfile.ZipFile("channel.zip")
+readme = io.TextIOWrapper(zip_file.open('readme.txt','r'))
+nothing = 0
+for i in readme.readlines():
+   print(i.strip())
+   if i.find('start') != -1:
+       nothing = i.split(" ")[-1].strip()
 
-for i in a:
-    print(''.join([char*(int(num)) for char,num in i]))
+comments = []
+run = True
+while run:
+    comments.append(zip_file.getinfo(nothing+'.txt'))
+    text_file = io.TextIOWrapper(zip_file.open(nothing+'.txt','r'))
+    s = text_file.read()
+    if s.find('nothing') != -1:
+       nothing = s.split(" ")[-1].strip()
+    else:
+        print(s)
+        run = False
+
+comments
+print(''.join([c.comment.decode() for c in comments]))
